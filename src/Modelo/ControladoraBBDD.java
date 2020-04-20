67579d9d1d39a571a9f3d1966e24e488ad7c989c
danie <danie@EquipoDeDani>
2020-04-20 14:13:16 +0200

    Cambio en el acceso a la BBDD utilizando datos.ini

--- src/Modelo/ControladoraBBDD.java
+++ src/Modelo/ControladoraBBDD.java
@@ -1,52 +1,80 @@
 package Modelo;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class ControladoraBBDD {
 
-	private String bd;
-	private String url= "jdbc:oracle:thin:@localhost:1521:XE";
-	private String usr = "SYSTEM";
-	private String pwd = "Miro_5838";
+	private String url= "";
+	private String user = "";
+	private String pwd = "";
+	private String usr = "";
 	private Connection conexion;
 	
 	public ControladoraBBDD()  {
 
+		Properties propiedades = new Properties();
+		InputStream entrada = null;
+		
+		try {
+			File miFichero = new File("src/Modelo/datos.ini");
+			if (miFichero.exists()){
+				System.out.println("entra");
+				entrada = new FileInputStream(miFichero);
+				propiedades.load(entrada);
+				url=propiedades.getProperty("url");
+				user=propiedades.getProperty("user");
+				pwd=propiedades.getProperty("pwd");
+				usr=propiedades.getProperty("usr");
+			}
+
+			else
+				System.out.println("Fichero no encontrado");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}finally {
+			if (entrada != null) {
+				try {
+					entrada.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conexion = DriverManager.getConnection(url, usr, pwd);
+			conexion = DriverManager.getConnection(url, user, pwd);
 
-			if(!conexion.isClosed()) {
-				System.out.println("Conexión establecida");
-
-				//conexion.close();
-			}
-			else
-				System.out.println("Fallo en Conexión");
+			if(conexion.isClosed())
+				System.out.println("Fallo en Conexión con la Base de Datos");
 
 
 		}catch (Exception e) {
 			System.out.println("ERROR en conexión con ORACLE");
 			e.printStackTrace();
 		}
-
-		
 	}
+		
 	public ObservableList<Persona>  ConsultaPersonas() throws SQLException{
 		//Preparo la conexión para ejecutar sentencias SQL de tipo update
 
 		ObservableList<Persona> listaPersonas =  FXCollections.observableArrayList();
 		
 		Statement stm = conexion.createStatement();
-		String selectsql = "SELECT NOMBRE, APELLIDO, EMAIL, SEXO, CASADO FROM DANIEL.PERSONAS";
+		String selectsql = "SELECT NOMBRE, APELLIDO, EMAIL, SEXO, CASADO FROM "+usr+".PERSONAS";
 
 		ResultSet resultado = stm.executeQuery(selectsql);
 	
@@ -78,7 +106,7 @@ public class ControladoraBBDD {
 		Statement stm = conexion.createStatement();
 		
 		try{
-			String insertsql = "INSERT INTO DANIEL.PERSONAS VALUES (?,?,?,?,?)";
+			String insertsql = "INSERT INTO "+usr+".PERSONAS VALUES (?,?,?,?,?)";
 
 			String auxcasado = "N";
 			if(persona.isCasado() == true){
@@ -116,7 +144,7 @@ public class ControladoraBBDD {
 		Statement stm = conexion.createStatement();
 		
 		try{
-			String insertsql = "DELETE DANIEL.PERSONAS WHERE EMAIL=?";
+			String insertsql = "DELETE "+usr+".PERSONAS WHERE EMAIL=?";
 			
 			PreparedStatement pstmt = conexion.prepareStatement (insertsql);
 			
@@ -142,7 +170,7 @@ public class ControladoraBBDD {
 		Statement stm = conexion.createStatement();
 
 		try{
-			String insertsql = "UPDATE DANIEL.PERSONAS SET NOMBRE=?, APELLIDO =?, EMAIL =?, SEXO =?, CASADO =? WHERE EMAIL=?";
+			String insertsql = "UPDATE "+usr+".PERSONAS SET NOMBRE=?, APELLIDO =?, EMAIL =?, SEXO =?, CASADO =? WHERE EMAIL=?";
 
 			String auxcasado = "N";
 			if(persona2.isCasado() == true){
@@ -177,7 +205,7 @@ public class ControladoraBBDD {
 		ObservableList<Persona> listaPersonas =  FXCollections.observableArrayList();
 		Statement stm = conexion.createStatement();
 		
-		String selectsql = "SELECT NOMBRE, APELLIDO, EMAIL, SEXO, CASADO FROM DANIEL.PERSONAS WHERE APELLIDO=?";
+		String selectsql = "SELECT NOMBRE, APELLIDO, EMAIL, SEXO, CASADO FROM "+usr+".PERSONAS WHERE APELLIDO=?";
 
 		PreparedStatement pstmt = conexion.prepareStatement (selectsql);
 		pstmt.setString(1, apellidoBusqueda);
